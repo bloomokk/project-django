1da33a47ee56539a566ebd59dc61d75b6f40682d
студент211 <211@mveu.local>
2024-09-25 11:02:38 +0400

    practic5

--- mysate/library/admin.py
+++ mysate/library/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from library.models import Author, Publisher, Book
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+  pass
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+  pass
+
+
+@admin.register(Publisher)
+class PublisherAdmin(admin.ModelAdmin):
+  pass
--- mysate/library/models/__init__.py
+++ mysate/library/models/__init__.py
@@ -2,6 +2,8 @@ from .author import Author
 from .book import Book
 from .publisher import Publisher
 
-
-__all__ = ('Author', 'Book', 'Publisher',
-           )
+__all__ = [
+  'Author',
+  'Book',
+  'Publisher'
+]
